2b0bd13713e39c4af564fdd156722d9405cd3b27
Riad Younes <riad.younes@hotmail.com>
2024-04-28 20:11:27 -0300

    feat: create invite

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,7 +18,9 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { createInvite } from './routes/invites/create-invite'
 import { getMembers } from './routes/members/get-members'
+import { removeMember } from './routes/members/remove-member'
 import { updateMember } from './routes/members/update-member'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
@@ -97,6 +99,10 @@ app.register(updateProject)
 // members
 app.register(getMembers)
 app.register(updateMember)
+app.register(removeMember)
+
+// invites
+app.register(createInvite)
 
 app.listen({ port: env.SERVER_PORT }).then(() => {
   console.log('HTTP server running')
