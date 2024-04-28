674e6ef386c21c9175c2ca1ebe6aa0fdf4f8975e
Riad Younes <riad.younes@hotmail.com>
2024-04-28 13:16:12 -0300

    feat: delete project

--- apps/api/src/http/routes/projects/delete-project.ts
+++ apps/api/src/http/routes/projects/delete-project.ts
@@ -19,7 +19,7 @@ export async function deleteProject(app: FastifyInstance) {
       {
         schema: {
           tags: ['project'],
-          summary: 'delete a project',
+          summary: 'Delete a project',
           security: [{ bearerAuth: [] }],
           params: z.object({
             slug: z.string(),
