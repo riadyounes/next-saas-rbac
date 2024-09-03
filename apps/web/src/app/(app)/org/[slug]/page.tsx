27373d695d4741b33c3d1627d486cad9cf16c5cf
Riad Younes <riad.younes@hotmail.com>
2024-09-03 16:40:18 -0300

    shutdown organization

---
+++ apps/web/src/app/(app)/org/[slug]/page.tsx
@@ -0,0 +1,7 @@
+export default async function Projects() {
+  return (
+    <div className="space-y-4">
+      <h1>Projects</h1>
+    </div>
+  )
+}
---
+++ apps/web/src/app/(app)/org/[slug]/project/[project]/page.tsx
@@ -0,0 +1,7 @@
+export default async function Projects() {
+  return (
+    <div className="space-y-4">
+      <h1>Project</h1>
+    </div>
+  )
+}
